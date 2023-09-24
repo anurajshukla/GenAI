a9c29130d8831d384a5a4dd7a1cc5e70db503fdf
Anuraj Shukla <89661673+anurajshukla@users.noreply.github.com>
2023-09-25 03:44:07 +0530

    updated endpoint

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -18,7 +18,7 @@ const   CreatePost = () => {
     if(form.prompt){
       try {
         setgeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        const response = await fetch('https://genai-smay.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -44,7 +44,7 @@ const   CreatePost = () => {
       setloading(true);
 
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://genai-smay.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
